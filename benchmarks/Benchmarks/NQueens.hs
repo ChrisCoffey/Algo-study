d288874c2d2fe715c1a500f320d4eed7db7b2715
Chris Coffey <cmcof273@gmail.com>
2019-06-15 13:52:09 -0400

    With benchmarks

--- benchmarks/Benchmarks/NQueens.hs
+++ benchmarks/Benchmarks/NQueens.hs
@@ -9,11 +9,11 @@ import Criterion.Main
 
 benchmarks :: Benchmark
 benchmarks = bgroup "NQueens" [
-     bench "naive 5x5" $ whnf nQueens1 5
-    ,bench "naive 8x8" $ whnf nQueens1 8
-    ,bench "naive 10x10" $ whnf nQueens1 10
+     bench "naive 5x5" $ whnf (length . nQueens1) 5
+    ,bench "naive 8x8" $ whnf (length . nQueens1) 8
+    ,bench "naive 10x10" $ whnf (length . nQueens1) 10
 
-    ,bench "matrix 5x5" $ whnf nQueens2 5
-    ,bench "matrix 8x8" $ whnf nQueens2 8
-    ,bench "matrix 10x10" $ whnf nQueens2 10
+    ,bench "matrix 5x5" $ whnf (length . nQueens2) 5
+    ,bench "matrix 8x8" $ whnf (length . nQueens2) 8
+    ,bench "matrix 10x10" $ whnf (length . nQueens2) 10
     ]
