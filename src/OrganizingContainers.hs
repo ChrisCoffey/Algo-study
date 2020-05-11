2c9c971ddd6b4bf9ad30f385694ab61ce69a89fb
Chris Coffey <cmcof273@gmail.com>
2020-05-11 10:10:19 -0400

    Defer mapping balls to buckets until the very end

--- src/OrganizingContainers.hs
+++ src/OrganizingContainers.hs
@@ -7,24 +7,21 @@ import qualified Data.Map as M
 
 organizingContainers :: [[Int]] -> Bool
 organizingContainers container = let
-    sums = zipWith (-) incorrectCounts freeCounts
+    sums = zipWith (-) (sort incorrectCounts) (sort freeCounts)
     in all (== 0) sums
     where
         arrContainers = A.listArray (0, n-1) $ A.listArray (0, n-1) <$> container
         n = length container
-        incorrectCounts = incorrectPerBucket <$> [0..n-1]
-        freeCounts = freeBalls <$> [0..n-1]
+        range = [0..n-1]
+        incorrectCounts = incorrectPerBucket <$> range
+        freeCounts = freeBalls <$> range
 
         -- all values in the bucket that are not of the same type as the bucket
         incorrectPerBucket idx = let
             bucket = arrContainers A.! idx
-            range = rangeExceptIndex idx
             in sum $ (bucket A.!) <$> range
 
         -- all values of type t in the matrix, that don't fall on row n
         freeBalls idx = let
-            range = rangeExceptIndex idx
-            vals = (\i -> (A.! idx) $ arrContainers A.! i)<$> range
+            vals = (\i -> (A.! idx) $ arrContainers A.! i) <$> range
             in sum vals
-
-        rangeExceptIndex idx = [x | x <- [0..n-1], x /= idx]
