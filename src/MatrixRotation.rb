8a3ddf0497763ce3049aa646cbea4732514bb867
Chris Coffey <cmcof273@gmail.com>
2020-05-28 09:40:59 -0400

    Piecemeal progress

--- src/MatrixRotation.rb
+++ src/MatrixRotation.rb
@@ -2,13 +2,28 @@
 
 require 'stringio'
 
+mnr = gets.rstrip.split
+
+$m = mnr[0].to_i
+
+$n = mnr[1].to_i
+
+r = mnr[2].to_i
+
+
+matrix = Array.new($m)
+
+$m.times do |i|
+    matrix[i] = gets.rstrip.split.map(&:to_i)
+end
+
 # Complete the matrixRotation function below.
 def matrixRotation(matrix, r)
-  resultMatrix = Array.new(m) { |_| Array.new size: n }
+  resultMatrix = Array.new($m) { |_| Array.new($n) }
 
-  (0..m).each do |i|
-    (0..n).each do |j|
-      y,x = computeOffset i, j, r
+  (0..$m-1).each do |i|
+    (0..$n-1).each do |j|
+      x,y = computeOffset i, j, r
       resultMatrix[y][x] = matrix[i][j]
     end
   end
@@ -19,6 +34,7 @@ end
 def computeOffset(x,y, r)
   lay = layer(x,y)
 
+  #   0,0 is the upper left of layer 1
   #
   #    ul-----------ur
   #    |            |
@@ -27,49 +43,87 @@ def computeOffset(x,y, r)
   #    ll-----------lr
   #
   ul_x,ul_y = [lay,lay]
-  ll_x,ll_y = [lay, m-lay]
-  lr_x,lr_y = [n-lay, m-lay]
-  ur_x,ur_y = [n-lay, lay]
+  ll_x,ll_y = [lay, $m-lay-1]
+  lr_x,lr_y = [$n-lay-1, $m-lay-1]
+  ur_x,ur_y = [$n-lay-1, lay]
 
-  perimeter = 2*( (m-lay) + (n-lay) )
+  perimeter = 2*( ($m-lay) + ($n-lay) )
   rotationDistance = r % perimeter
 
-  # determine where the point is
+  puts [x,y,r,rotationDistance, lay].inspect
+  # determine where the point is, then move it counter-clockwise until either
+  #   a) r == 0. This means the new location is on this row
+  #   b) it reaches a boundary and needs to move in the next direction. In this case
+  #      recursively call computeOffset with the new x,y, & remaining r
+
+  # top row
+  if y == ul_y
+    distanceFromUpperLeft = x - ul_x
+    distanceToMove = distanceFromUpperLeft > rotationDistance ? rotationDistance : (rotationDistance - distanceFromUpperLeft)
+    if distanceToMove > distanceFromUpperLeft
+      computeOffset ul_x, (y+1), (distanceToMove - 1)
+    else
+      [x - distanceToMove, y]
+    end
+  # left side
+  elsif x == ll_x && y != ul_y && y != ll_y
+    distanceFromLowerLeft = ll_y - y
+    distanceToMove = distanceFromLowerLeft > rotationDistance ? rotationDistance : (rotationDistance - distanceFromLowerLeft)
+    if distanceToMove > distanceFromLowerLeft
+      computeOffset (x+1), ll_y, (distanceToMove -1)
+    else
+      [x, y + distanceToMove]
+    end
+  # bottom
+  elsif y == ll_y
+    distanceFromLowerRight = lr_x - x
+    distanceToMove = distanceFromLowerRight > rotationDistance ? rotationDistance : (rotationDistance - distanceFromLowerRight)
+    if distanceToMove > distanceFromLowerRight
+      computeOffset lr_x, (y-1), (distanceToMove -1)
+    else
+      [x + distanceToMove, y]
+    end
+  # right side
+  elsif x == ur_x && y != lr_y && y != ur_y
+    distanceFromUpperRight = y - ur_y
+    distanceToMove = distanceFromUpperRight > rotationDistance ? rotationDistance : (rotationDistance - distanceFromUpperRight)
+    if distanceToMove > distanceFromUpperRight
+      computeOffset (x-1), ur_y, (distanceToMove-1)
+    else
+      [x, y - distanceToMove]
+    end
+  else
+    puts "Failure"
+  end
+
+  # how to handle corners?
+  # corners count as part of the horizontal
   # Move it around the perimeter until distance = 0, using "jumps" rather than decrementing everything by 1
 
   # return the new point
 end
 
-def layer(x,y)
-  xMidpoint = n/2
-  yMidpoint = m/2
 
-  yLayer = yMidpoint - (yMidpoint - y).abs
-  xLayer = xMidpoint - (xMidpoint - x).abs
+def layer(x,y)
+  rhs,lhs = [0, $n-1]
+  top,bottom = [0, $m-1]
+  xMidpoint = $n/2
+  yMidpoint = $m/2
+
+  xLayer = x > xMidpoint ? lhs - x : x
+  yLayer = y > yMidpoint ? bottom - y : y
 
   [yLayer, xLayer].min
 end
 
 def printMatrix(matrix)
+  puts "\n------------------\n"
   matrix.each do |row|
-    puts row.join(" ")
+    puts row.inspect
+    #puts row.join(" ")
   end
 end
 
-mnr = gets.rstrip.split
-
-m = mnr[0].to_i
-
-n = mnr[1].to_i
-
-r = mnr[2].to_i
-
-
-matrix = Array.new(m)
-
-m.times do |i|
-    matrix[i] = gets.rstrip.split.map(&:to_i)
-end
 
 matrixRotation matrix, r
 
